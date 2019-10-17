1f5983d70ef7d4b97b883d1564a0eb0b94fa49ce
Steven Bal <steven@maykinmedia.nl>
2019-10-17 10:21:12 +0200

    Changed static url to /static/

--- src/vng/conf/base.py
+++ src/vng/conf/base.py
@@ -224,7 +224,7 @@ LOCALE_PATHS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/atvstatic/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
